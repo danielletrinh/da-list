b85a116ad674aa36aa9b899a32c1bf99bd89490d
danielle trinh <trinhdanielle45@gmail.com>
2025-08-04 20:46:23 +0200

    changed maison coffee jpeg

--- app/page.tsx
+++ app/page.tsx
@@ -535,7 +535,7 @@ export default function Home() {
                         : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                     }`}
                   >
-                    {region === 'all' ? 'all regions' : region === 'recommended' ? 'recommended' : region}
+                    {region === 'all' ? 'full alphabetical list' : region === 'recommended' ? 'recommended' : region}
                     <span className="ml-2 text-xs">
                       ‹ {getFilteredRegionCount(region)} ›
                     </span>
